5b1719310173c74bd5abddc23225bf2642033b3f
FoolycoolyZ <98475149+FoolycoolyZ@users.noreply.github.com>
2024-09-18 11:09:21 -0700

    Update musicloop.js

--- musicloop.js
+++ musicloop.js
@@ -1,12 +1,5 @@
-window.onload = setInterval(GameLoop, 1000 / 10); //10fps
-
-let myAudio = new Audio();
-
-myAudio.src = 'muisc_Nujabes_-_Aruarian_Dance.mp3';
-
-function Gameloop() {
-    myAudio.play();
-    if (myAudio.paused == true) {
-        myAudio.play();
-    }
-}
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+  });
